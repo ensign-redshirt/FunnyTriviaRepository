02299eb50fa41f776af2f6c9a7f38ea08e4a38f7
ensign-redshirt <alewis3@mail.umw.edu>
2014-03-14 11:19:11 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from collections import defaultdict
-import utils, MySQLdb
+import utils, MySQLdb, random
 app = Flask(__name__)                                                                                                                                                              
                                                                                                                                                                                    
 @app.route('/')                                                                                                                                                                    
@@ -12,25 +12,51 @@ def report():
   return render_template('findtrivia.html', selectedMenu='trivia')                                                                                                                     
                                                                                                                                                                                    
 @app.route('/trivia2', methods=['POST'])                                                                                                                                           
-def trivia2():                                                                                                                                                                     
+def trivia2():     
+  db = utils.db_connect()
+  cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
   trivia =  request.form.get("triviatype")
   if trivia == 'Laws':
     tCol = 'laws'
+    cur.execute('select content, state from ' + tCol + ';')
   elif trivia == 'Trivia':
     tCol = 'trivia'
+    cur.execute('select content from '+ tCol + ';' )
   elif trivia == 'Sayings':
     tCol = 'sayings'
-  elif trivia == 'Quotes':
+    cur.execute('select content, author from ' + tCol + ';')
+  elif trivia == 'Memes':
     tCol = 'memes'
+    cur.execute('select content from ' + tCol + ';')
   elif trivia == 'Fortune Cookies':
     tCol = 'fortuneCookies'
-  db = utils.db_connect()
-  cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-  cur.execute('select content from ' + tCol + ';')
+    cur.execute('select content from ' + tCol + ';')
   rows = cur.fetchall()
   print rows
-  return render_template('triviadsiplay.html', trivia=trivia, rows=rows)
+  return render_template('triviadisplay.html', trivia=trivia, rows=rows)
 
+@app.route('/random', methods=['POST'])                                                                                                                                           
+def random():     
+  db = utils.db_connect()
+  cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+  trivia =  request.form.get("triviatype")
+  ranNum = random.randint(0,4)
+  
+  if ranNum == 1:
+    tCol = 'laws'
+    cur.execute('select content,state from '+tCol+' order by rand() limit 1')
+  elif ranNum == 2:
+    tCol = 'trivia'
+    cur.execute('select content,state from '+tCol+' order by rand() limit 1')
+  elif ranNum == 3:
+    tCol = 'sayings'
+    cur.execute('select content,state from '+tCol+' order by rand() limit 1')
+  elif ranNum == 4:  
+    tCol = 'fortuneCookies'
+    cur.execute('select content,state from '+tCol+' order by rand() limit 1')
+  print rows
+  
+  return render_template('triviadisplay.html', trivia = trivia, rows = rows)
 if __name__ == '__main__':                                                                                                                                                         
     app.debug=True                                                                                                                                                                 
     app.run(host='0.0.0.0', port=3000)                                                                                                                                
